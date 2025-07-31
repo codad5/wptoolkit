0cc9c23b11084696fb2233ece3c29558fb3d55fa
codad5 <aniezeoformic@gmail.com>
2025-07-31 07:43:28 +0100

    fix

--- src/Utils/Ajax.php
+++ src/Utils/Ajax.php
@@ -700,6 +700,11 @@ final class Ajax
 
 		// Get current file path (e.g., C:/laragon/www/wp-content/plugins/my-plugin/src/Utils/Ajax.php)
 		$current_file_path = __FILE__;
+        $script_path = '/assets/js/wptoolkit-ajax.js';
+
+        Debugger::varDump( 'nile-ent', [
+            'current_file_path' => $current_file_path,
+        ]);
 
 
 		// Find wp-content position and extract the relative path
@@ -715,12 +720,15 @@ final class Ajax
 		// Navigate from Ajax.php location to assets/js/ajax.js
 		$path_parts = explode('/', dirname($relative_path));
 
-		// Remove the last 2 parts (src/Utils) to get to root
-		array_pop($path_parts); // Remove 'Utils'
-		array_pop($path_parts); // Remove 'src'
+        //Remove 'Utils' and 'src'
+
+        // if path parts contains 'Utils' and 'src', remove them
+        $path_parts = array_filter($path_parts, function ($part) {
+            return $part !== 'Utils' && $part !== 'src';
+        });
 
 		// Build the script path
-		$script_relative_path = implode('/', $path_parts) . '/assets/js/wptoolkit-ajax.js';
+		$script_relative_path = implode('/', $path_parts) . $script_path ;
 
 		// Convert to full URL
 		$script_url = site_url('/' . $script_relative_path);
